cd8efc20830f6332a2b855168e35dea87d6d9ee6
Hesoo Cha <hesoocha@Camdens-MacBook-Pro.local>
2023-04-20 10:58:49 -0400

    fixed cart api route but now server error look into users more

--- server/db/models/Cart.js
+++ server/db/models/Cart.js
@@ -1,12 +1,7 @@
 const Sequelize = require('sequelize')
 const db = require('../db')
-const jwt = require('jsonwebtoken')
-const bcrypt = require('bcrypt');
 
 const Cart = db.define('cart', {
-  items: {
-    type: Sequelize.ARRAY,
-  },
   totalPrice: {
     type: Sequelize.INTEGER,
     allowNull: false,
