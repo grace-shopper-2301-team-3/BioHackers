57545d8874c5be3247f78d2e5cafcd208ffc0e0d
Ashley H <ashleyho887@gmail.com>
2023-04-20 14:04:54 -0700

    adding main theme into App

--- client/app/App.js
+++ client/app/App.js
@@ -1,13 +1,20 @@
 import React from "react";
 import Navbar from "../features/navbar/Navbar";
 import AppRoutes from "./AppRoutes";
+import biohackersTheme from "./theme";
+import { ThemeProvider } from "@mui/material"
+import { MainContainer } from "../features/style/StyleGuide";
+import { CssBaseline } from "@mui/material";
 
 const App = () => {
   return (
-    <div>
+    <ThemeProvider theme={biohackersTheme}>
+      <CssBaseline />
+    <MainContainer>
       <Navbar />
       <AppRoutes />
-    </div>
+    </MainContainer>
+    </ThemeProvider>
   );
 };
 
