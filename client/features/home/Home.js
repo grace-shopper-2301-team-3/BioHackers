7b4b10f50b34b21e60056574effa2c4b7ca76720
Ashley H <ashleyho887@gmail.com>
2023-04-20 19:13:34 -0700

    placeholders on home component

--- client/features/home/Home.js
+++ client/features/home/Home.js
@@ -1,12 +1,23 @@
 import React from "react";
 import biohackersTheme from "../../app/theme";
 import { MainContainer } from "../style/StyleGuide";
-import { ThemeProvider, Typography, Container, Box } from "@mui/material";
+import {
+  ThemeProvider,
+  Typography,
+  Container,
+  Box,
+  Card,
+  CardMedia,
+  CardContent,
+} from "@mui/material";
 
 const Home = () => {
   return (
     <ThemeProvider theme={biohackersTheme}>
       <MainContainer sx={{ position: "relative" }}>
+
+{/* Main Heading for Homepage */}
+
         <Container sx={{ position: "relative", overflow: "hidden" }}>
           <img
             src="https://media0.giphy.com/media/4knozU8q9AXvpod9qy/giphy.gif?cid=ecf05e479xiha122z06g45ujmxa7rc8xx3f47wwrdj1hrj64&rid=giphy.gif&ct=g"
@@ -29,7 +40,10 @@ const Home = () => {
             Biohackers
           </Typography>
         </Container>
-        <Container sx={{py:4}}>
+
+{/* Categories Section */}
+
+        <Container sx={{ py: 4 }}>
           <Typography
             variant="h2"
             sx={{
@@ -42,9 +56,54 @@ const Home = () => {
           >
             Categories
           </Typography>
-          <Box></Box>
+          <Box
+            sx={{
+              display: "flex",
+              flexDirection: "row",
+              gap: "20px",
+              justifyContent: "space-evenly",
+              margin: "20px",
+            }}
+          >
+            <Card sx={{ maxWidth: 500 }}>
+              <CardMedia component="img" image="https://picsum.photos/500" />
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="div">
+                  Category 1
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                  Some description
+                </Typography>
+              </CardContent>
+            </Card>
+            <Card sx={{ maxWidth: 500 }}>
+              <CardMedia component="img" image="https://picsum.photos/500" />
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="div">
+                  Category 1
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                  Some description
+                </Typography>
+              </CardContent>
+            </Card>
+            <Card sx={{ maxWidth: 500 }}>
+              <CardMedia component="img" image="https://picsum.photos/500" />
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="div">
+                  Category 1
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                  Some description
+                </Typography>
+              </CardContent>
+            </Card>
+          </Box>
         </Container>
-        <Container sx={{py:4}}>
+
+{/* Top Sellers Section */}
+
+        <Container sx={{ py: 4 }}>
           <Typography
             variant="h2"
             sx={{
@@ -57,7 +116,82 @@ const Home = () => {
           >
             Top Sellers
           </Typography>
-          <Box></Box>
+          <Box
+            sx={{
+              display: "flex",
+              flexDirection: "row",
+              gap: "20px",
+              justifyContent: "space-evenly",
+              margin: "20px",
+            }}
+          >
+            <Card sx={{ maxWidth: 150 }}>
+              <CardMedia component="img" image="https://picsum.photos/150" />
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="div">
+                  Category 1
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                  Some description
+                </Typography>
+              </CardContent>
+            </Card>
+            <Card sx={{ maxWidth: 150 }}>
+              <CardMedia component="img" image="https://picsum.photos/150" />
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="div">
+                  Category 1
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                  Some description
+                </Typography>
+              </CardContent>
+            </Card>
+            <Card sx={{ maxWidth: 150 }}>
+              <CardMedia component="img" image="https://picsum.photos/150" />
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="div">
+                  Category 1
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                  Some description
+                </Typography>
+              </CardContent>
+            </Card>
+            <Card sx={{ maxWidth: 150 }}>
+              <CardMedia component="img" image="https://picsum.photos/150" />
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="div">
+                  Category 1
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                  Some description
+                </Typography>
+              </CardContent>
+            </Card>
+            <Card sx={{ maxWidth: 150 }}>
+              <CardMedia component="img" image="https://picsum.photos/150" />
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="div">
+                  Category 1
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                  Some description
+                </Typography>
+              </CardContent>
+            </Card>
+            <Card sx={{ maxWidth: 150 }}>
+              <CardMedia component="img" image="https://picsum.photos/150" />
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="div">
+                  Category 1
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                  Some description
+                </Typography>
+              </CardContent>
+            </Card>
+          </Box>{" "}
         </Container>
       </MainContainer>
     </ThemeProvider>
