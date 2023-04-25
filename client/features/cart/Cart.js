833c7fbb0862bae5db0fdf3f7ffb862d453756e2
Ashley H <ashleyho887@gmail.com>
2023-04-25 11:06:22 -0700

    styling cart

--- client/features/cart/Cart.js
+++ client/features/cart/Cart.js
@@ -4,57 +4,85 @@ import { useDispatch, useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 import CartItem from "./CartItem";
 import { fetchCart } from "./cartSlice";
-import { Button } from "@mui/material";
+import { ThemeProvider, Typography, Container, Box } from "@mui/material";
+import biohackersTheme from "../../app/theme";
+import { HeroButton, MainContainer } from "../style/StyleGuide";
 
 
 const Cart = () => {
-  const dispatch = useDispatch();
-  const cart = useSelector((state) => state.cart);
-  const [totalPrice, setTotalPrice] = useState(0);
-  const [cartUpdated, setCartUpdated] = useState(false);
+ const dispatch = useDispatch();
+ const cart = useSelector((state) => state.cart);
+ const [totalPrice, setTotalPrice] = useState(0);
+ const [cartUpdated, setCartUpdated] = useState(false);
 
 
-  useEffect(() => {
-    if (cart.length) {
-      const cartArray = Array.from(cart);
-      const total = cartArray.reduce((acc, curr) => {
-        const itemTotal = curr.product.productPrice * curr.quantity;
-        console.log({itemTotal, itemPrice: curr.product.productPrice, quantity: curr.quantity})
-        return acc + itemTotal;
-      }, 0);
-      setTotalPrice(total);
-    } else {
-      setTotalPrice(0);
-    }
-  }, [cart, cartUpdated]);
+ useEffect(() => {
+   if (cart.length) {
+     const cartArray = Array.from(cart);
+     const total = cartArray.reduce((acc, curr) => {
+       const itemTotal = curr.product.productPrice * curr.quantity;
+       console.log({
+         itemTotal,
+         itemPrice: curr.product.productPrice,
+         quantity: curr.quantity,
+       });
+       return acc + itemTotal;
+     }, 0);
+     setTotalPrice(total);
+   } else {
+     setTotalPrice(0);
+   }
+ }, [cart, cartUpdated]);
 
-  useEffect(() => {
-    dispatch(fetchCart());
-  }, [dispatch]);
 
-  return (
-    <div className="cartContainer">
-      <p>Your Shopping Cart</p>
-        {Array.isArray(cart) && cart.length ? cart.map((cartItem) => (
-          <div key={cartItem.id}>
-            <CartItem cartItem={cartItem} />
-          </div>
-        ))
-      ) : (
-        <p>Your cart is empty</p>
-      )}
-      {cart.length ? (
-        <div key={cart.id} className="checkoutContainer">
-          <p>total: USD ${totalPrice}</p>
-          <Link to="/checkout">
-            <Button variant="contained">Check Out</Button>
-          </Link>
-        </div>
-      ) : (
-        <></>
-      )}
-    </div>
-  );
+ useEffect(() => {
+   dispatch(fetchCart());
+ }, [dispatch]);
+
+
+ return (
+   <ThemeProvider theme={biohackersTheme}>
+     <Container sx={{ mx: 2, textAlign: "center" }}>
+       <Typography variant="h4">Your Shopping Cart</Typography>
+     </Container>
+     <MainContainer sx={{ mx: 2, display: "flex" }}>
+       <Box style={{ flex: 3 }}>
+         {Array.isArray(cart) && cart.length ? (
+           cart.map((cartItem) => (
+             <Box key={cartItem.id}>
+               <CartItem cartItem={cartItem} />
+             </Box>
+           ))
+         ) : (
+           <Typography variant="h2">Your cart is empty</Typography>
+         )}
+       </Box>
+       <Container style={{ flex: 1 }}>
+         {cart.length ? (
+           <Box key={cart.id} className="checkoutContainer" sx={{ mt: 2, p: 3, border: "1px solid", borderImage: "linear-gradient(45deg, #7F00FF, #00bfff, #ff00ff) 1", boxShadow: "0 0px 20px #7F00FF" }}>
+             <Typography variant="h4">Order Summary</Typography>
+             <Typography variant="body1" color="primary.light">Free Shipping</Typography>
+             <Box sx={{ my: 4}}>
+             <Typography variant="body1">Total in USD </Typography>
+             <Typography variant="h5" color="secondary.dark" sx={{ my: 2, textAlign: "right"}}>{totalPrice.toLocaleString("en-US", {
+                 style: "currency",
+                 currency: "USD",
+               })}</Typography>
+             </Box>
+             <Box sx={{ m: 4}}>
+             <Link to="/checkout">
+               <HeroButton variant="contained"><Typography variant="body1" sx={{ textTransform: "uppercase"}}>Checkout</Typography></HeroButton>
+             </Link>
+             </Box>
+           </Box>
+         ) : (
+           <></>
+         )}
+       </Container>
+     </MainContainer>
+   </ThemeProvider>
+ );
 };
+
 
 export default Cart;
